b9d686d888d15c4e45e87714781ed41b72c3b252
Marco Antognini <antognini.marco@gmail.com>
2014-11-29 11:56:09 +0100

    Translated community.php

--- community-fr.php
+++ community-fr.php
@@ -1,60 +1,54 @@
 <?php
-    $breadcrumbs = array();
+    $breadcrumbs = array
+    (
+        'Communauté' => 'community.php'
+    );
     include('header-fr.php');
 ?>
 
-<h1>Simple and Fast Multimedia Library</h1>
-
-<!--<div class="news">
-    <strong>SFML 2.1</strong> est sortie</a>
-</div>-->
+<h1>Communauté</h1>
 
 <div class="home-section">
     <div class="column">
-        <div class="title">SFML est multi-media</div>
+        <div class="title">&raquo; <a href="http://fr.sfml-dev.org/forums/" title="Aller au forum">Forum</a></div>
         <p>
-            SFML offre une interface simple vers les différents composants de votre PC, afin de faciliter le développement de jeux ou d'applications multimedia. Elle se
-            compose de cinq modules : système, fenêtrage, graphisme, audio et réseau.
+            Que vous ayez une question au sujet de l'<a href="learn-fr.php" title="Aller à la page des tutoriels et de la documentation">API</a> SFML, que vous rencontriez des comportements étranges avec SFML ou que vous ayez une suggestion de nouvelles fonctionnalités, vous trouverez de l'aide, des réponses ou un feedback sur le forum.
         </p>
         <p>
-            Découvrez plus en détail leurs fonctionnalités, dans <a href="learn-fr.php" title="Aller à la page des tutoriels et de la documentation">les tutoriels et
-            la documentation de l'API</a>.
+            Dans le sous-forum <a href="http://fr.sfml-dev.org/forums/index.php?board=24.0" title="Aller au sous-forum 'Projets SFML'">Projets SFML</a> vous trouvez de nombreux projets exitants.
         </p>
     </div>
-    <div class="column image">
-        <img src="<?php echo image('home/multimedia.png') ?>" alt="Image multimedia" />
+    <div class="column">
+        <img src="<?php echo image('community/forum.png') ?>" alt="Forum" />
     </div>
 </div>
 
 <div class="home-section">
-    <div class="column image">
-        <img src="<?php echo image('home/multiplatform.png') ?>" alt="Image multi-platforme" />
+    <div class="column">
+        <img src="<?php echo image('community/wiki.png') ?>" alt="Wiki" />
     </div>
     <div class="column">
-        <div class="title">SFML est multi-plateforme</div>
+        <div class="title">&raquo; <a href="https://github.com/LaurentGomila/SFML/wiki" title="Go to the wiki">Wiki</a></div>
         <p>
-            Avec SFML, votre application peut se compiler et tourner sans effort sur la plupart des systèmes d'exploitation : Windows, Linux, Mac OS X et prochainement
-            Android &amp; iOS.
-        </p>
-        <p>
-            Des SDKs précompilés pour votre OS préféré sont disponibles sur la <a href="download-fr.php" title="Aller à la page des téléchargements">page des téléchargements</a>.
+            Le wiki de la communauté SFML contient des extraits de code, des tutoriels et d'autres contenus fruits de la collaboration de la communauté.
         </p>
     </div>
 </div>
 
 <div class="home-section">
     <div class="column">
-        <div class="title">SFML est multi-langage</div>
+        <div class="title">&raquo; <a href="https://kiwiirc.com/client/irc.boxbox.org/sfml" target="_blank" title="Chat in the browser now">IRC</a></div>
         <p>
-            SFML possède des bindings officiels pour les langages C et .Net. Et grace à sa communauté très active, elle est également disponible dans beaucoup d'autres
-            langages tels que Java, Ruby, Python, Go et plus encore.
+            Pour une aide amicale et des discussions sur SFML (mais aussi des chats hors-sujet, avec un grand nombre d'utilisateurs), cliquez sur le bouton suivant pour discuter instantanément dans votre naviguateur, ou connectez vous avec votre client IRC favori au serveur décrit ci-dessous.
         </p>
-        <p>
-            Apprenez-en d'avantage à leur sujet sur la <a href="download/bindings-fr.php" title="Aller à la page des bindings">page des bindings</a>.
-        </p>
+        <ul>
+            <li>SSL: <a href="irc://boxbox.org:6697/#sfml" title="Ouvrir un client IRC avec le port SSL">irc://boxbox.org:6697/#sfml</a></li>
+            <li>Non SSL: <a href="irc://boxbox.org:6667/#sfml" title="Ouvrir un client IRC avec le port non SSL">irc://boxbox.org:6667/#sfml</a></li>
+            <li><a href="https://kiwiirc.com/client/irc.boxbox.org/sfml" target="_blank" title="Discutez dans votre naviguateur maintenant">Discutez dans votre naviguateur maintenant</a></li>
+        </ul>
     </div>
-    <div class="column image">
-        <img src="<?php echo image('home/multilanguage.png') ?>" alt="Image multi-langage" />
+    <div class="column">
+        <a href="https://kiwiirc.com/client/irc.boxbox.org/sfml" target="_blank"><img src="<?php echo image('community/irc.png') ?>" alt="IRC" /></a>
     </div>
 </div>
 
