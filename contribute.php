91fb0ac903293147169e47d466670635ca1d7ff2
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2014-12-02 21:17:09 +0100

    Added the contribution guide.

---
+++ contribute.php
@@ -0,0 +1,93 @@
+<?php
+    $breadcrumbs = array
+    (
+        'Development' => 'development.php',
+        'Contribution Guidelines' => 'contribute.php'
+    );
+    include('header.php');
+?>
+
+<h1>Contribution Guidelines</h1>
+<p>You would like to see a feature implemented or a bug fixed in SFML? Great! Contributions to SFML are highly appreciated, be it in the form of general ideas, concrete suggestions or code patches. </p>
+<p>This document explains how you can contribute. By carefully reading it, you minimize the time it takes on your and on our side until a modification is implemented. Since we can't (and don't want to) implement everything, the following guidelines clarify what it takes for a contribution to be accepted.</p>
+
+<h1>General considerations</h1>
+<p>In case of any questions or doubt (and after reading available resources), feel free to ask on the forum. Before opening GitHub issues and/or pull requests, also use the forum to discuss your matter.</p>
+
+<h2>Scope</h2>
+<p>Contributions must fit into one of the following SFML modules:</p>
+<ul>
+ <li>System</li>
+ <li>Window</li>
+ <li>Graphics</li>
+ <li>Audio</li>
+ <li>Network</li>
+</ul>
+<p>That clearly out rules anything specifically related to game development. Such <strong>negative examples</strong> are: Physics, collision detection/response, AI, path finding, 3D abstraction layer (like the current graphics module), file system, etc.</p>
+<p>There are several reasons why SFML does not support these features:</p>
+<ul>
+ <li>The API should be focused and of high quality. Rather than implementing a lot of features that are possible and sometimes even simple to implement, we consider it important that people have a clear view of what this library provides.</li>
+ <li>For most of the listed points, there are existing libraries that have been developed over years, and that provide everything you need.</li>
+ <li>Resources are limited. The time we would invest into other features can be used to improve and extend the existing multimedia library.</li>
+ <li>How these features could be provided is controversial. Many of them are very specific and concrete implementations are often limited to a few use cases.</li>
+ <li>Many features can be added as an extension. SFML works as an abstraction of low-level and platform-specific functionality to provide a portable API. Higher-level features do not have this requirement.</li>
+</ul>
+<p>The rule of thumb is: if a feature can easily be added on top of SFML without modifying the core library, it is probably not a good candidate to be integrated.</p>
+
+<h2>Code style</h2>
+<p>Contributions in the form of code must follow our <a href="style.php" title="Go to the style guide"><strong>syntactical conventions</strong></a>. Before submitting a pull request, please make sure your commits really adhere to them. For design and semantics (e.g. "do I use a pointer or a reference", "what is the correct type here"), have a look at existing SFML source code.</p>
+
+<h2>Philosophy</h2>
+<p>SFML has been developed under the following considerations; keep them in mind when contributing.</p>
+<ul>
+ <li><strong>Simple.</strong> Features are easy to use. Even more complex functionality should be provided in the most simple way possible, using abstraction where necessary.</li>
+ <li><strong>Slim API.</strong> Minimal APIs are preferred to bloated collections of functionality that try to fit everyone's needs. Especially when introducing new functionality, it is often a good idea to start with a small set of classes and functions. It can still be extended later.</li>
+ <li><strong>Fast.</strong> Performance-critical tasks are implemented efficiently. Readability and correctness take precedence though, and premature optimization is to be avoided.</li>
+ <li><strong>Clean code.</strong> Source code is well-structured, modular, readable and uses modern C++ idioms. Avoid clever hacks or micro-optimizations.</li>
+ <li><strong>Pragmatic.</strong> SFML does not attempt to solve every possible problem, it rather tries to offer functionality that is useful for a wide variety of people.</li>
+</ul>
+
+<h1>Contributing</h1>
+<h2>Reporting bugs</h2>
+<ol>
+ <li>Make sure the bug is not already known. Check <a href="https://github.com/LaurentGomila/SFML/issues">the GitHub issues</a> and <a href="http://en.sfml-dev.org/forums/">the forum</a>.</li>
+ <li>Install the <a href="https://github.com/LaurentGomila/SFML">latest source</a> from GitHub and check if the bug has not already been fixed. See the <a href="http://www.sfml-dev.org/learn.php"><em>Compiling SFML with CMake</em> tutorial</a> for assistance.</li>
+ <li>Open a new thread on the <a href="http://en.sfml-dev.org/forums/">forum</a> in the appropriate help board (e.g. if it's about audio, go to the <em>Help/Audio</em> board) and give the following information:
+  <ul>
+   <li><a href="http://en.sfml-dev.org/forums/index.php?topic=5559.msg36368#msg36368">Minimal complete code</a> that makes the bug appear.</li>
+   <li>Instructions for reproducing the bug.</li>
+   <li>Environment information (operating system, SFML version, compiler etc.).</li>
+   <li>Screenshots/videos, if relevant, to show certain behavior.</li>
+   <li>Anything else that helps identify the problem.</li>
+  </ul>
+ </li>
+</ol>
+
+<h2>Requesting features</h2>
+<p>If you've got an idea for a new feature, and it's within SFML's scope (see above), create a proper thread on <a href="http://en.sfml-dev.org/forums/index.php?board=2.0">the dedicated sub-forum</a>.</p>
+<p>A feature request should always contain a rationale for the change. Whenever you suggest a feature, argue from a user perspective: in which way is SFML limited? What use cases does the feature allow? How can users beside you benefit from the change?</p>
+
+<h2>Testing</h2>
+<p>Testing is a crucial part of integrating new changes – the more people who help us, the faster we can proceed with development, and the quicker we can find and eliminate bugs.</p>
+<p>Feel free to participate in forum discussions, GitHub issues or existing pull requests. Or try out the latest changes from the master Git branch and report back any issues that you may find in the forum.</p>
+
+<h2>Sending patches/pull requests</h2>
+<p>In general code is only accepted when a proper GitHub issue already exists (usually preceded by a forum discussion). Make sure that nobody else is already working on an issue, or simply offer help and wait for a response.</p>
+<p>Patches are accepted through GitHub pull requests. These are the steps required to send in such a request:</p>
+<ol>
+ <li><a href="https://github.com/LaurentGomila/SFML/fork">Fork SFML</a>.</li>
+ <li>Commit your changes and push them to your forked repository.</li>
+ <li>Create a <a href="https://help.github.com/articles/creating-a-pull-request">pull request</a> that targets the <em>master</em> branch.</li>
+</ol>
+<p>In the pull request, provide the following information:</p>
+<ul>
+ <li>Descriptive title.</li>
+ <li>Concise description of the modification.</li>
+ <li>Reference the GitHub issue that you implemented/fixed.</li>
+ <li>Link to the forum thread where the discussion originated.</li>
+ <li><a href="http://en.sfml-dev.org/forums/index.php?topic=5559.msg36368#msg36368">Minimal complete code</a> snippet that allows to test the implementation. The idea is that anybody can test the functionality with minimal effort. If external files are needed, add a link to them.</li>
+ <li>Bugfix: the code should reproduce the bug.</li>
+ <li>Feature: show how the feature is used.</li>
+</ul>
+<p>For smaller issues, a single commit should be attached to the pull request. If you implement a larger modification, you may split the commits, but please do so semantically ("implemented feature X, fixed bug Y") and not chronologically ("started X, continued Y, finished Z"). Keep in mind that you can not only add new commits, but also overwrite existing ones by force-pushing (<code>git push --force-with-lease</code>) to your repository branch.</p>
+<p>We may ask you to elaborate and improve certain parts of your code, sometimes we also edit it directly. We consider it important that you are listed as a contributor in the SFML commit history, so make sure that you choose credentials (author name and e-mail address) that you want to be published.</p>
