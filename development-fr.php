fea359a48f3067a19ab361c94b762be714a6d2a6
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2014-12-02 21:12:55 +0100

    Updated the development page slightly.

--- development-fr.php
+++ development-fr.php
@@ -16,37 +16,37 @@
 </div>
 
 <div class="link-box two-columns-right">
-    <a class="issues" href="issues.php">
+    <a class="issues" href="issues-fr.php">
         <div class="title">Issues</div>
         <div class="description">What to do if you have an issue</div>
     </a>
 </div>
 
 <div class="link-box two-columns-left">
-    <a class="contribute" href="contribute.php">
+    <a class="changelog" href="changelog-fr.php">
+        <div class="title">Changelog</div>
+        <div class="description">What has changed?</div>
+    </a>
+</div>
+
+<div class="link-box two-columns-right">
+    <a class="contribute" href="contribute-fr.php">
         <div class="title">Contribution Guidelines</div>
         <div class="description">How to help developing SFML</div>
     </a>
 </div>
 
-<div class="link-box two-columns-right">
-    <a class="workflow" href="workflow.php">
-        <div class="title">Our Workflow</div>
-        <div class="description">How we utilize Git</div>
-    </a>
-</div>
-
 <div class="link-box two-columns-left">
-    <a class="style" href="style.php">
+    <a class="style" href="style-fr.php">
         <div class="title">Code Style Guide</div>
         <div class="description">How to format your contributions</div>
     </a>
 </div>
 
 <div class="link-box two-columns-right">
-    <a class="license" href="license.php">
-        <div class="title">License</div>
-        <div class="description">SFML's license</div>
+    <a class="workflow" href="workflow-fr.php">
+        <div class="title">Our Workflow</div>
+        <div class="description">How we utilize Git</div>
     </a>
 </div>
 
