0d0efa00b48abfbeca3b3158fce7ce1a162eca87
Stefan Schindler <stefan@boxbox.org>
2014-12-11 12:16:58 +0100

    Redone bindings page.

--- download/bindings.php
+++ download/bindings.php
@@ -5,38 +5,9 @@
         'Bindings' => 'download/bindings.php'
     );
     include('../header.php');
-
-    function binding($name, $logo, $language, $authors, $active, $webpage)
-    {
-        $statusLabel = $active ? 'active' : 'inactive';
-        $statusClass = $active ? 'active' : 'inactive';
-        $id = trim(str_replace(' ', '-', preg_replace('/[^a-z0-9 -]+/', '', strtolower($name))), '-');
-
-        echo '<h2 id="' . $id . '"><a class="h2-link" href="#' . $id . '">' . $name . '</a><a class="back-to-top" href="#top" title="Top of the page"></a></h2>' . "\n";
-        echo '<table class="styled binding">' . "\n";
-        if (isset($logo))
-            echo '<tr><td class="header" colspan="2"><img src="../images/bindings/' . $logo .'"/></td></tr>' . "\n";
-        echo '<tr><td class="header">Status</td><td class="status-' . $statusClass . '">' . $statusLabel . '</td></tr>' . "\n";
-        echo '<tr><td class="header">Language</td><td class="language">' . $language . '</td></tr>' . "\n";
-        echo '<tr><td class="header">' . (count($authors) == 1 ? 'Author' : 'Authors') . '</td><td class="author">';
-        $i = 0;
-        foreach ($authors as $author => $email)
-        {
-            if (isset($email))
-                echo '<a href="mailto:' . $email . '" title="Contact ' . $author . '">' . $author . '</a>';
-            else
-                echo $author;
-            if ($i++ < count($authors) - 1)
-                echo ',';
-            echo ' ';
-        }
-        echo '</td></tr>' . "\n";
-        echo '<tr><td class="header">Web page</td><td class="webpage"><a href="' . $webpage . '" title="' . $name . ' official web page">' . $webpage . '</a></td></tr>' . "\n";
-        echo '</table>' . "\n";
-    }
 ?>
 
-<h1>SFML bindings</h1>
+<h1>Bindings</h1>
 
 <p>
     Here is a list of all the known SFML bindings. CSFML and SFML.Net are official bindings, the others are not: They are developed by SFML users.
@@ -46,23 +17,81 @@
     Feel free to <a href="mailto:laurent@sfml-dev.org" title="Contact the webmaster">contact me</a> if you want to update, add or remove information about an SFML binding.
 </p>
 
-<?php
+<table class="styled bindings" style="width: 100%;">
+    <thead>
+        <tr>
+            <th style="width: 25%;">Language</th>
+            <th style="width: 30%;">Name</th>
+            <th style="width: 45%;">Authors</th>
+        </tr>
+    </thead>
 
-    binding('CSFML', null, 'C', array('Laurent Gomila' => 'laurent@sfml-dev.org'), true, 'http://www.sfml-dev.org/download/csfml');
-    binding('SFML.Net', null, '.Net languages (C#, VB.Net, etc.)', array('Laurent Gomila' => 'laurent@sfml-dev.org'), true, 'http://www.sfml-dev.org/download/sfml.net');
-    binding('JSFML', 'jsfml.png', 'Java', array('Patrick Dinklage' => 'pdinklag@gmail.com'), true, 'http://jsfml.org');
-    binding('DerelictSFML2', null, 'D', array('Mike Parker' => 'aldacron@gmail.com'), true, 'https://github.com/aldacron/Derelict3/');
-    binding('DSFML', null, 'D', array('Jeremy DeHaan' => 'dehaan.jeremiah@gmail.com'), true, 'https://github.com/Jebbs/DSFML');
-    binding('pySFML', 'python-sfml.png', 'Python', array('Jonathan De Wachter' => 'dewachter.jonathan@gmail.com', 'Edwin Marshall' => null), true, 'http://www.python-sfml.org/');
-    binding('rbSFML', 'rbsfml.png', 'Ruby', array('Henrik Valter Vogelius Hansson' => 'groogy@groogy.se'), true, 'http://www.groogy.se/mainsite/rbsfml/');
-    binding('Ocsfml', null, 'OCaml', array('Jun Maillard' => null, 'Kenji Maillard' => null), true, 'https://github.com/JoeDralliam/Ocsfml');
-    binding('ocaml-sfml', null, 'OCaml', array('Florent Monnier' => null), true, 'http://ocaml-sfml.forge.ocamlcore.org/');
-    binding('GoSFML2', null, 'Go', array('krepa098' => null), true, 'https://bitbucket.org/krepa098/gosfml2');
-    binding('nimrod-sfml', null, 'Nimrod', array('fowl' => null), true, 'https://github.com/fowlmouth/nimrod-sfml');
-    binding('EuSFML2', null, 'Euphoria', array('Andy Patterson' => null), true, 'http://www.rapideuphoria.com/eusfml.zip');
-    binding('rust-sfml', null, 'Rust', array('Jérémy Letang' => 'letang.jeremy@gmail.com'), true, 'http://www.rust-sfml.org/');
-
-?>
+    <tbody>
+        <tr>
+            <th rowspan="1">C</th>
+            <td><a href="http://www.sfml-dev.org/download/csfml" class="website">CSFML</a></td>
+            <td><a href="mailto:laurent@sfml-dev.org">Laurent Gomila</a></td>
+        </tr>
+        <tr>
+            <th rowspan="1">.Net (C#, VB.Net etc.)</th>
+            <td><a href="http://www.sfml-dev.org/download/sfml.net" class="website">SFML.Net</a></td>
+            <td><a href="mailto:laurent@sfml-dev.org">Laurent Gomila</a></td>
+        </tr>
+        <tr>
+            <th rowspan="1">Java</th>
+            <td><a href="http://jsfml.org/" class="website"><img src="../images/bindings/jsfml.png" alt="JSFML"/></a></td>
+            <td><a href="mailto:pdinklag@gmail.com">Patrick Dinklage</a></td>
+        </tr>
+        <tr>
+            <th rowspan="2">D</th>
+            <td><a href="https://github.com/DerelictOrg/DerelictSFML2" class="website">DerelictSFML2</a></td>
+            <td><a href="mailto:aldacron@gmail.com">Mike Parker</a></td>
+        </tr>
+        <tr>
+            <td><a href="https://github.com/Jebbs/DSFML" class="website">DSFML</a></td>
+            <td><a href="mailto:dehaan.jeremiah@gmail.com">Jeremy DeHaan</a></td>
+        </tr>
+        <tr>
+            <th rowspan="1">Python</th>
+            <td><a href="http://www.python-sfml.org/" class="website"><img src="../images/bindings/python-sfml.png" alt="pySFML"></a></td>
+            <td><a href="mailto:dewachter.jonathan@gmail.com">Jonathan De Wachter</a>, Edwin Marshall</td>
+        </tr>
+        <tr>
+            <th rowspan="1">Ruby</th>
+            <td><a href="http://www.groogy.se/mainsite/rbsfml/" class="website"><img src="../images/bindings/rbsfml.png" alt="rbSFML"></a></td>
+            <td><a href="mailto:groogy@groogy.se">Henrik Valter Vogelius Hansson</a></td>
+        </tr>
+        <tr>
+            <th rowspan="2">OCaml</th>
+            <td><a href="https://github.com/JoeDralliam/Ocsfml" class="website">Ocsfml</a></td>
+            <td>Jun Maillard, Kenji Maillard</td>
+        </tr>
+        <tr>
+            <td><a href="http://ocaml-sfml.forge.ocamlcore.org/" class="website">ocaml-sfml</a></td>
+            <td>Florent Monnier</td>
+        </tr>
+        <tr>
+            <th rowspan="1">Go</th>
+            <td><a href="https://bitbucket.org/krepa098/gosfml2" class="website">GoSFML2</a></td>
+            <td>krepa098</td>
+        </tr>
+        <tr>
+            <th rowspan="1">Nimrod</th>
+            <td><a href="https://github.com/fowlmouth/nimrod-sfml" class="website">nimrod-sfml</a></td>
+            <td>fowl</td>
+        </tr>
+        <tr>
+            <th rowspan="1">Euphoria</th>
+            <td><a href="http://www.rapideuphoria.com/eusfml.zip" class="website">EuSFML2</a></td>
+            <td>Andy Patterson</td>
+        </tr>
+        <tr>
+            <th rowspan="1">Rust</th>
+            <td><a href="http://www.rust-sfml.org/" class="website">rust-sfml</a></td>
+            <td><a href="letang.jeremy@gmail.com">Jérémy Letang</a></td>
+        </tr>
+    </tbody>
+</table>
 
 <?php
     require("../footer.php");
