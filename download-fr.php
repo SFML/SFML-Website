3d32caf9d26fb7b4edce24d9e201699b624bbbac
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2014-12-15 22:02:30 +0100

    Centered the SFML 2.2 download button and unified the 'Older versions' texts.

--- download-fr.php
+++ download-fr.php
@@ -8,37 +8,37 @@
 
 <h1>Téléchargement</h1>
 
-<div class="link-box two-columns-left">
+<div class="link-box one-column">
     <a class="download" href="download/sfml/2.2/index-fr.php">
         <div class="title">SFML 2.2</div>
         <div class="description">Dernière version stable</div>
     </a>
 </div>
 
-<div class="link-box two-columns-right">
+<div class="link-box two-columns-left">
     <a class="bindings" href="download/bindings-fr.php">
         <div class="title">Bindings</div>
         <div class="description">SFML dans d'autres langages</div>
     </a>
 </div>
 
-<div class="link-box two-columns-left">
+<div class="link-box two-columns-right">
     <a class="repository" href="https://github.com/LaurentGomila/SFML/">
         <div class="title">Git dépôt</div>
         <div class="description">GitHub.com</div>
     </a>
 </div>
 
-<div class="link-box two-columns-right">
+<div class="link-box two-columns-left">
     <a class="goodies" href="download/goodies/index-fr.php">
         <div class="title">Goodies</div>
         <div class="description">Logos</div>
     </a>
 </div>
 
-<div class="link-box two-columns-left">
+<div class="link-box two-columns-right">
     <a class="download-old" href="download/sfml/old-versions-fr.php">
-        <div class="title">Autres versions</div>
+        <div class="title">Anciennes versions</div>
         <div class="description">SFML 1.6 et vieilles 2.x</div>
     </a>
 </div>
--- download/sfml/old-versions.php
+++ download/sfml/old-versions.php
@@ -7,7 +7,7 @@
     include('../../header.php');
 ?>
 
-<h1>Download old versions of SFML</h1>
+<h1>Download older versions of SFML</h1>
 
 <div class="link-box one-column">
     <a class="download-old" href="2.1">
