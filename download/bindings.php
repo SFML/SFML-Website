4049489240fd4994a202d41cb8af94d13cf6b801
Stefan Schindler <stefan@boxbox.org>
2016-03-25 18:26:22 +0100

    Split bindings into active and outdated sections.

--- download/bindings.php
+++ download/bindings.php
@@ -17,6 +17,8 @@
     Feel free to <a href="mailto:webmaster@sfml-dev.org" title="Contact the webmaster">contact me</a> if you want to update, add or remove information about an SFML binding.
 </p>
 
+<h2>Active and up-to-date</h2>
+
 <table class="styled bindings" style="width: 100%;">
     <thead>
         <tr>
@@ -35,24 +37,62 @@
             <td><a href="mailto:laurent@sfml-dev.org">Laurent Gomila</a></td>
         </tr>
         <tr>
-            <th rowspan="1">.Net (C#, VB.Net etc.)</th>
-            <td><a href="sfml.net" class="website">SFML.Net</a></td>
-            <td>2.2</td>
-            <td><a href="mailto:laurent@sfml-dev.org">Laurent Gomila</a></td>
-        </tr>
-        <tr>
             <th rowspan="1">Crystal</th>
             <td><a href="https://github.com/BlaXpirit/crsfml" class="website"><img src="../images/bindings/crsfml.png" alt="CrSFML"/></a></td>
             <td>2.3</td>
             <td><a href="mailto:blaxpirit@gmail.com">Oleh Prypin</a></td>
         </tr>
         <tr>
-            <th rowspan="2">D</th>
+            <th>D</th>
             <td><a href="https://github.com/DerelictOrg/DerelictSFML2" class="website">DerelictSFML2</a></td>
             <td>2.3</td>
             <td><a href="mailto:aldacron@gmail.com">Mike Parker</a></td>
         </tr>
         <tr>
+            <th>Haskell</th>
+            <td>
+                <a href="https://github.com/SFML-haskell/SFML" class="website"><img src="../images/bindings/hssfml.png" style="vertical-align:middle"></a>
+                (<a href="https://hackage.haskell.org/package/SFML">Hackage</a>)
+            </td>
+            <td>2.3</td>
+            <td><a href="mailto:jeannekamikaze@gmail.com">Marc Sunet</a>, <a href="mailto:alfredo.dinapoli@gmail.com">Alfredo Di Napoli</a></td>
+        </tr>
+        <tr>
+            <th rowspan="1">Nim</th>
+            <td><a href="https://github.com/BlaXpirit/nim-csfml" class="website">nim-csfml</a></td>
+            <td>2.3</td>
+            <td><a href="mailto:blaxpirit@gmail.com">Oleh Prypin</a></td>
+        </tr>
+        <tr>
+            <th rowspan="1">Pascal</th>
+            <td><a href="https://github.com/CWBudde/PasSFML" class="website">PasSFML</a></td>
+            <td>2.3</td>
+            <td>Christian-W. Budde</td>
+        </tr>
+    </tbody>
+</table>
+
+<h2>Outdated/abandoned</h2>
+
+<table class="styled bindings" style="width: 100%;">
+    <thead>
+        <tr>
+            <th style="width: 25%;">Language</th>
+            <th style="width: 30%;">Name</th>
+            <th style="width: 10%;">SFML</th>
+            <th style="width: 35%;">Authors</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <th rowspan="1">.Net (C#, VB.Net etc.)</th>
+            <td><a href="sfml.net" class="website">SFML.Net</a></td>
+            <td>2.2</td>
+            <td><a href="mailto:laurent@sfml-dev.org">Laurent Gomila</a></td>
+        </tr>
+        <tr>
+            <th>D</th>
             <td><a href="http://www.dsfml.com/" class="website">DSFML</a></td>
             <td>2.1</td>
             <td><a href="mailto:dehaan.jeremiah@gmail.com">Jeremy DeHaan</a></td>
@@ -70,15 +110,6 @@
             <td>krepa098</td>
         </tr>
         <tr>
-            <th rowspan="1">Haskell</th>
-            <td>
-                <a href="https://github.com/SFML-haskell/SFML" class="website"><img src="../images/bindings/hssfml.png" style="vertical-align:middle"></a>
-                (<a href="https://hackage.haskell.org/package/SFML">Hackage</a>)
-            </td>
-            <td>2.3</td>
-            <td><a href="mailto:jeannekamikaze@gmail.com">Marc Sunet</a>, <a href="mailto:alfredo.dinapoli@gmail.com">Alfredo Di Napoli</a></td>
-        </tr>
-        <tr>
             <th rowspan="1">Java</th>
             <td><a href="http://jsfml.org/" class="website"><img src="../images/bindings/jsfml.png" alt="JSFML"/></a></td>
             <td>2.2</td>
@@ -91,12 +122,6 @@
             <td>Zachary Yedidia</td>
         </tr>
         <tr>
-            <th rowspan="1">Nim</th>
-            <td><a href="https://github.com/BlaXpirit/nim-csfml" class="website">nim-csfml</a></td>
-            <td>2.3</td>
-            <td><a href="mailto:blaxpirit@gmail.com">Oleh Prypin</a></td>
-        </tr>
-        <tr>
             <th rowspan="2">OCaml</th>
             <td><a href="https://github.com/JoeDralliam/Ocsfml" class="website">Ocsfml</a></td>
             <td>2.2</td>
@@ -106,12 +131,6 @@
             <td><a href="http://ocaml-sfml.forge.ocamlcore.org/" class="website">ocaml-sfml</a></td>
             <td>2.0</td>
             <td>Florent Monnier</td>
-        </tr>
-        <tr>
-            <th rowspan="1">Pascal</th>
-            <td><a href="https://github.com/CWBudde/PasSFML" class="website">PasSFML</a></td>
-            <td>2.3</td>
-            <td>Christian-W. Budde</td>
         </tr>
         <tr>
             <th rowspan="1">Python</th>
