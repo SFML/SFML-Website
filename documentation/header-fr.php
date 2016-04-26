00c310f0ff5b21c6dfea6285d33d89f2b15cbe09
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2016-04-26 08:37:08 +0200

    Added 'View latest version' button to the documentation section.

--- documentation/header-fr.php
+++ documentation/header-fr.php
@@ -1,4 +1,6 @@
 <?php
+    $latest = '2.3.2';
+    $linklatest = '';
     $doxygen = true;
     $docpath = 'documentation/' . $version . '-fr/';
     $breadcrumbs = array
@@ -8,6 +10,12 @@
         $pagetitle => substr($_SERVER['REQUEST_URI'], 1) // remove the starting '/'
     );
     require('../../header-fr.php');
+    
+    if($version != $latest)
+    {
+        $linklatest = '<a class="important" href="../' . $latest . '-fr/' . str_replace('documentation/' . $version . '-fr/', '', $breadcrumbs[$pagetitle]) . '"><strong>Attention:</strong> cette page se réfère à une ancienne version de SFML. Cliquez ici pour passer à la dernière version.</a>';
+    }
 ?>
 
-<h1>Documentation de SFML <?php echo $version ?></h1>
+<h1>Documentation de SFML <?php echo $version; ?></h1>
+<?php echo $linklatest; ?>
--- documentation/header.php
+++ documentation/header.php
@@ -1,4 +1,6 @@
 <?php
+    $latest = '2.3.2';
+    $linklatest = '';
     $doxygen = true;
     $docpath = 'documentation/' . $version . '/';
     $breadcrumbs = array
@@ -8,6 +10,12 @@
         $pagetitle => substr($_SERVER['REQUEST_URI'], 1) // remove the starting '/'
     );
     require('../../header.php');
+    
+    if($version != $latest)
+    {
+        $linklatest = '<a class="important" href="../' . $latest . '/' . str_replace('documentation/' . $version . '/', '', $breadcrumbs[$pagetitle]) . '"><strong>Warning:</strong> this page refers to an old version of SFML. Click here to switch to the latest version.</a>';
+    }
 ?>
 
-<h1>Documentation of SFML <?php echo $version ?></h1>
+<h1>Documentation of SFML <?php echo $version; ?></h1>
+<?php echo $linklatest; ?>
