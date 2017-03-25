3b200f1818ef0ae97bab1f725a49114998a65ff2
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2017-03-25 23:25:44 +0100

    Updated the Rust binding for the French page.

--- download/bindings-fr.php
+++ download/bindings-fr.php
@@ -134,7 +134,7 @@
         <tr>
             <th rowspan="1">Rust</th>
             <td><a href="https://github.com/jeremyletang/rust-sfml" class="website"><img src="../images/bindings/rust-sfml.png" alt="rust-sfml"></a></td>
-            <td>2.3</td>
+            <td>2.4</td>
             <td><a href="mailto:letang.jeremy@gmail.com">Jérémy Letang</a></td>
         </tr>
 
