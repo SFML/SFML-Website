3b967ec479080a8919cfedffb9a0733d5fc155f4
Stefan Schindler <stefan@boxbox.org>
2022-04-19 21:57:28 +0200

    Drop Piwik from footer.

--- footer.php
+++ footer.php
@@ -6,20 +6,5 @@
                 Copyright © <a href="mailto:laurent@sfml-dev.org" title="Contact the author">Laurent Gomila</a>
             </div>
         </div>
-        <script type="text/javascript">
-            var _paq = _paq || [];
-            _paq.push(["setCookieDomain", "*.sfml-dev.org"]);
-            _paq.push(["setDomains", ["*.sfml-dev.org","*.en.sfml-dev.org","*.fr.sfml-dev.org","*.www.sfml-dev.org"]]);
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-                var u="https://www.sfml-dev.org/piwik/";
-                _paq.push(['setTrackerUrl', u+'piwik.php']);
-                _paq.push(['setSiteId', '1']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-            })();
-        </script>
-        <noscript><p><img src="https://www.sfml-dev.org/piwik/piwik.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
     </body>
 </html>
