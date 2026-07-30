b0d6282fc3a05c3904a7ac98b3eab43dd9e7fdf7
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2026-07-30 16:16:44 +0200

    Update bundle references

--- pages/javascripts/bundle.js
+++ pages/javascripts/bundle.js
@@ -1,3 +1,3 @@
 const script = document.createElement("script");
-script.src = "../../assets/javascripts/bundle.79ae519e.min.js";
+script.src = "../../assets/javascripts/bundle.d7400e89.min.js";
 document.head.appendChild(script);
